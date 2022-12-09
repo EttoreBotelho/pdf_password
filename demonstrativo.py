f2ef205646b636021b36662e79237788d3e0ccdd
Ettore Botelho <ettorebotelho@hotmail.com>
2022-12-09 10:42:22 -0300

    update 09-12-22

--- demonstrativo.py
+++ demonstrativo.py
@@ -2,12 +2,14 @@ from PyPDF2 import PdfReader, PdfWriter
 import pandas as pd
 import re
 
+
 nome_arquivo = input('Digite o Nome do Arquivo a ser Segmentado: ')
 mes_ano_ref = input('Digite o Mês e Ano de Referência do Demonstrativo (Ex: 10 2022): ')
 
 senhas = pd.read_excel('pdf/senhas.xlsx')
 reader = PdfReader(f'pdf/{nome_arquivo}.pdf')
 writer = PdfWriter('')
+
 count = 0 
 for page in reader.pages:
     texto = page.extract_text()
@@ -20,6 +22,7 @@ for page in reader.pages:
                 writer.encrypt(str(senha))
                 writer.write(e)
                 count += 1
+                
 print('======================================')
 print(f'         {count} de {str(reader.numPages)} PDFs Salvos')
 print('======================================')
